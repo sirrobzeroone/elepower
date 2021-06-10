bfb33c580563372e2fc90e0ad86bc5aa94060464
Sirrobzeroone <44497598+sirrobzeroone@users.noreply.github.com>
2021-06-10 19:19:07 +1000

    Fix alpha texture warning
    Change for  5.4 mesh/nodebox nodes now need a valid "use_texture_alpha" setting or logs fill up with warnings.

--- elepower_dynamics/conduits.lua
+++ elepower_dynamics/conduits.lua
@@ -3,6 +3,7 @@
 ele.register_conduit("elepower_dynamics:conduit", {
 	description = "Power Conduit",
 	tiles = {"elepower_conduit.png"},
+	use_texture_alpha = "clip",
 	groups = {oddly_breakable_by_hand = 1, cracky = 1}
 })
 
@@ -10,6 +11,7 @@ ele.register_conduit("elepower_dynamics:conduit", {
 fluid_lib.register_transfer_node("elepower_dynamics:opaque_duct", {
 	description = "Opaque Fluid Duct",
 	tiles = {"elepower_opaque_duct.png"},
+	use_texture_alpha = "clip",
 	duct_density = 1/5,
 	groups = {oddly_breakable_by_hand = 1, cracky = 1}
 })
